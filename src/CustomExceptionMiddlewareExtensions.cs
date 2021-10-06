3f9d22a1c8c56d532975924b13861862858db3b5
Isaac Nunes Borges <isaac.borges@avenuecode.com>
2021-10-06 19:08:11 -0300

    Create CustomExceptionMiddlewareExtensionsTests

--- src/CustomExceptionMiddlewareExtensions.cs
+++ src/CustomExceptionMiddlewareExtensions.cs
@@ -9,28 +9,18 @@ namespace CustomExceptionMiddleware
     {
         public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder app)
         {
-            ValidateAppBuilder(app);
-
             return app.UseMiddleware<CustomExceptionMiddleware>();
         }
 
-        public static IApplicationBuilder UseCustomExceptionMiddleware(
-            this IApplicationBuilder app,
-            CustomExceptionOptions options)
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder app, CustomExceptionOptions options)
         {
-            ValidateAppBuilder(app);
             ValidateOptions(options);
 
             return app.UseMiddleware<CustomExceptionMiddleware>(Options.Create(options));
         }
 
-        public static IApplicationBuilder UseCustomExceptionMiddleware(
-            this IApplicationBuilder app,
-            Action<CustomExceptionOptions> configureOptions)
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder app, Action<CustomExceptionOptions> configureOptions)
         {
-            ValidateAppBuilder(app);
-            ValidateActionOptions(configureOptions);
-
             CustomExceptionOptions options;
             using (var scope = app.ApplicationServices.CreateScope())
             {
@@ -41,27 +31,11 @@ namespace CustomExceptionMiddleware
             return app.UseMiddleware<CustomExceptionMiddleware>(Options.Create(options));
         }
 
-        private static void ValidateAppBuilder(IApplicationBuilder app)
-        {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
-        }
-
         private static void ValidateOptions(CustomExceptionOptions options)
         {
             if (options == null)
             {
                 throw new ArgumentNullException(nameof(options));
-            }
-        }
-
-        private static void ValidateActionOptions(Action<CustomExceptionOptions> configureOptions)
-        {
-            if (configureOptions == null)
-            {
-                throw new ArgumentNullException(nameof(configureOptions));
             }
         }
     }
---
+++ tests/CustomExceptionMiddlewareExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomExceptionMiddleware.Tests
+{
+    public class CustomExceptionMiddlewareExtensionsTests
+    {
+        [Fact(DisplayName = "Should throw argument null exception when create null custom exception options")]
+        public async Task UseCustomExceptionMiddleware_CreateNullOptions_ShouldThrowArgumentNullException()
+        {
+            // Act
+            Func<Task> act = async () => 
+            {
+                await new HostBuilder()
+                                .ConfigureWebHost(webBuilder =>
+                                {
+                                    webBuilder
+                                        .UseTestServer()
+                                        .ConfigureServices(services => { })
+                                        .Configure(app =>
+                                        {
+                                            CustomExceptionOptions customExceptionOptions = null;
+                                            app.UseCustomExceptionMiddleware(customExceptionOptions);
+                                        });
+                                })
+                                .StartAsync();
+            };
+
+            // Assert
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+    }
+}
